c8921613d9e85f72b8fe9228472dd53696ca95b0
n0808 <life.with.chums.119@gmail.com>
2020-07-31 10:55:36 +0900

    commit

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,7 @@
 class Message < ApplicationRecord
   belongs_to :group
   belongs_to :user
-  
-  
+  validates :ondo, presence: true
+  validates :Textarea, presence: true
   validates :symptom, presence: true, inclusion: { in: 1..2 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   root 'groups#index'
   resources :users, only: [:edit, :update]
   resources :groups, only: [:index, :new, :create, :edit, :update] 
-  resources :members, only: [:index,:edit]
+  resources :members, only: [:index,:edit] do
+    resources :messages, only: [:index, :create]
+  end
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
